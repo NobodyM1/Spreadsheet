4ec8c49adaf05dbcc942b3beea3cb7d3fe381f87
somename <someemail>
2016-10-07 15:34:17 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,5 +1,10 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Spreadsheet {
+	
+	String cell;
+	String value;
 
 	public String get(String cell) {
 		// to be implemented
@@ -7,11 +12,36 @@ public class Spreadsheet {
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		int lenght = 0;
+		if(0 > cell.length() && cell.length() > 2){
+			//Error
+		}
+		if(value.matches("\'.*\'") == false){
+			//Value must be numbers only
+			Pattern p = Pattern.compile("-?\\d+");
+			Matcher m = p.matcher(value);
+			while (m.find()){
+				lenght++;
+			}
+			if(value.length() != lenght){
+				//Error
+				this.value = "#Error";
+			}
+			this.value = value;
+			
+		}
+		else {
+			//Get the value between ''
+			Pattern pattern = Pattern.compile("\'([^\"]*)\'");
+			Matcher matcher = pattern.matcher(value);
+			while(matcher.find()){
+				this.value = matcher.group(1);
+			}
+		}
 	}
 	
 	public String evaluate(String cell) {
-		// to be implemented
+		
 		return null;
 	}
 	
