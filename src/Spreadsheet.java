834426eec40bed40ba2bccb56aa1c6ad699d2414
somename <someemail>
2016-10-07 16:13:06 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -15,7 +15,7 @@ public class Spreadsheet {
 		int lenght = 0;
 		
 		
-		Pattern pat = Pattern.compile("e");
+		Pattern pat = Pattern.compile("a");
 		Matcher mat = pat.matcher(value);
 		int count = 0;
 		while (mat.find()){
