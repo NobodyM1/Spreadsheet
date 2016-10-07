f5b283a22fc5ec0111386a081e429d36f17d7af2
somename <someemail>
2016-10-07 16:41:16 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,12 +7,19 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test_123_equals_ 123() {
+	public void test_123_equals_123() {
 		Spreadsheet test = new Spreadsheet();
 		test.set("A1", "123");
 		String result = test.get("A1");
 		assertEquals("123", result);
-		
+	}
+	
+	@Test
+	public void test_double_quoted_returns_quoted_text() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "'test'");
+		String result = test.get("A1");
+		assertEquals("test", result);
 	}
 
 }
