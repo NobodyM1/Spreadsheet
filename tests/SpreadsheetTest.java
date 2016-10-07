b36418aaea221a25a3cd51eccc896a9864365f9a
somename <someemail>
2016-10-07 16:40:07 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test() {
+	public void test_123_equals_ 123() {
 		Spreadsheet test = new Spreadsheet();
-		test.set("A1", "-1");
+		test.set("A1", "123");
 		String result = test.get("A1");
-		assertEquals("-1", result);
+		assertEquals("123", result);
 		
 	}
 
