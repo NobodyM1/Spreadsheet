fdc526dd1dba770719c17f51298724506bfdc197
somename <someemail>
2016-10-07 15:37:37 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -9,7 +9,7 @@ public class SpreadsheetTest {
 	@Test
 	public void test() {
 		Spreadsheet test = new Spreadsheet();
-		test.set("A1", "15");
+		test.set("A1", "15k");
 		String result = test.get("A1");
 		assertEquals("15", result);
 		
