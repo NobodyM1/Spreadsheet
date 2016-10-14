a2eaff03cafa1d20635176a5c5b465735bc7add7
mireijon <mireijon@STUPID-PC713.stnet.local>
2016-10-14 17:04:41 +0300

    Assignment finished

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,65 +1,145 @@
 import java.util.HashMap;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class Spreadsheet {
 	 
-	String cell;
-	String value;
+	int count4 = 0;
+	boolean isOperator = false;
 	
 	//HashMap map = new HashMap();
 	
-
+	HashMap<String, String> hmap = new HashMap<String, String>();
+	
 	public String get(String cell) {
 		
 		//Object result = map.get(cell);
+		String test = hmap.get(cell);
 		
-		return value;
+		return test;
 	}
 	
 	public void set(String cell, String value) {
-		// Count how many ' can be found in value
+		hmap.put(cell, value);
+	}
+	
+	//*****************EVALUATION**********************
+	public String evaluate(String cell) {
+		
+		String test = hmap.get(cell);
+		
 		Pattern pat = Pattern.compile("'");
-		Matcher mat = pat.matcher(value);
+		Matcher mat = pat.matcher(test);
 		int count = 0;
+		
 		while (mat.find()){
 			count++;
 		}
 		
 		if(count < 2){
 			//Value must be numbers only
+			
+			//************** Check if value is reference *************
+			pat = Pattern.compile("=");
+			int count3 = 0;
+			mat = pat.matcher(test);
+			while(mat.find()){
+				count3++;
+			}
+			pat = Pattern.compile("((\\d*\\.\\d+)|(\\d+)|([\\+\\-\\*/\\(\\)]))");
+			mat = pat.matcher(test);
+			while(mat.find()){
+				isOperator = true;
+			}
+			if(isOperator == true){
+				String math = test.substring(1);
+				int reader = 0;
+				int number1 = 0;
+				char operator = '+';
+				while(reader < math.length()){
+					if(Character.isDigit(math.charAt(reader)) == true ){
+						if(operator == '+'){
+							number1 = number1 + Character.getNumericValue(math.charAt(reader));
+						}
+						if(operator == '-'){
+							number1 = number1 - Character.getNumericValue(math.charAt(reader));
+						}
+						if(operator == '*'){
+							number1 = number1 * Character.getNumericValue(math.charAt(reader));
+						}
+						if(operator == '/'){
+							number1 = number1 / Character.getNumericValue(math.charAt(reader));
+						}
+						if(operator == '%'){
+							number1 = number1 % Character.getNumericValue(math.charAt(reader));
+						}
+						reader++;
+					}
+					else{
+						operator = math.charAt(reader);
+						reader++;
+					}
+					
+				}
+				String result = String.valueOf(number1);
+				return result;
+			}
+			//Accepts only input that has no ' characters
+			if(count3 == 1 && count == 0){
+				String refCell = test.substring(1);
+				test = hmap.get(refCell);
+				//Loop back and check the new value
+				hmap.put(cell, test);
+				if(count4 < 1){
+					count4++;
+					evaluate(cell);
+				}
+				else{
+					return "#Circular";
+				}
+				
+			}
+			//*********** Value reference end ***************
+			
+			//********** Check if input is only digits ***********
 			int count2 = 0;
-			for (int i = 0, len = value.length(); i < len; i++) {
-			    if (Character.isDigit(value.charAt(i))) {
+			for (int i = 0, len = test.length(); i < len; i++) {
+			    if (Character.isDigit(test.charAt(i)) || test.charAt(i) == '-') {
 			        count2++;
 			    }
 			}
 			
-			if(value.length() != count2){
+			if(test.length() != count2){
 				//Error
-				this.value = "#Error";
+				// Change value 
+				//hmap.put(cell, "#Error");
+				return "#Error";
+				
 			}
 			else{
-				this.value = value;
+				//Nothing
+				//hmap.put(cell, test);
+				return test;
 			}
-			
-			
+			//********* Digit check ends **************
 		}
+		
+		//************* Gets value between '' ***************
 		else {
-			//Get the value between ''
 			Pattern pattern = Pattern.compile("\'([^\"]*)\'");
-			Matcher matcher = pattern.matcher(value);
+			Matcher matcher = pattern.matcher(test);
 			while(matcher.find()){
-				this.value = matcher.group(1);
+				test = matcher.group(1);
+				//hmap.put(cell, test);
+				return test;
 			}
 		}
-		
-		//map.put(this.cell, this.value);
-	}
-	
-	public String evaluate(String cell) {
-		
-		return null;
+		//**************** Value get ends ****************
+		return test;
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -6,6 +6,102 @@ import org.junit.Test;
 
 public class SpreadsheetTest {
 
+	
+	@Test
+	public void test_Setup() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "123");
+		test.evaluate("A1");
+		String result = test.get("A1");
+		assertEquals("123", result);
+	}
+	
+	@Test
+	public void test_Number_handling() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "-1");
+		test.evaluate("A1");
+		String result = test.get("A1");
+		assertEquals("-1", result);
+	}
+	
+	@Test
+	public void test_Wrongly_formatted_integers() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "5A");
+		String result = test.evaluate("A1");;
+		assertEquals("#Error", result);
+	}
+	
+	@Test
+	public void test_String_handling() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "'a string'");
+		String result = test.evaluate("A1");;
+		assertEquals("a string", result);
+	}
+	
+	@Test
+	public void test_Unquoted_strings() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "'a string");
+		String result = test.evaluate("A1");;
+		assertEquals("#Error", result);
+	}
+	
+	@Test
+	public void test_Simple_formulas() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "='a string'");
+		String result = test.evaluate("A1");;
+		assertEquals("a string", result);
+	}
+	
+	@Test
+	public void test_Simple_formulas_with_errors() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "='a string");
+		String result = test.evaluate("A1");;
+		assertEquals("#Error", result);
+	}
+	
+	@Test
+	public void test_Cell_references() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A5", "5");
+		test.set("A1", "=A5");
+		String result = test.evaluate("A1");
+		assertEquals("5", result);
+	}
+	
+	@Test
+	public void test_Errors_in_cell_references() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A5", "5A");
+		test.set("A1", "=A5");
+		String result = test.evaluate("A1");
+		assertEquals("#Error", result);
+	}
+	
+	@Test
+	public void test_Circular_references() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A5", "=A1");
+		test.set("A1", "=A5");
+		String result = test.evaluate("A1");
+		assertEquals("#Circular", result);
+	}
+	
+	@Test
+	public void test_Integer_operations() {
+		Spreadsheet test = new Spreadsheet();
+		test.set("A1", "=1+1*2");
+		String result = test.evaluate("A1");
+		assertEquals("4", result);
+	}
+	
+	
+	/*
 	@Test
 	public void test_123_equals_123() {
 		Spreadsheet test = new Spreadsheet();
@@ -53,6 +149,6 @@ public class SpreadsheetTest {
 		String result = test.get("A1");
 		assertEquals("#Error", result);
 	}
-	
+	*/
 	
 }
